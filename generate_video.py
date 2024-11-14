ae737fcc4fb18e2abb0a9f62fef044b4e3148d01
lingqiz <zlqzcc@gmail.com>
2024-11-14 09:13:29 -0500

    use command line argument

--- generate_video.py
+++ generate_video.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import numpy as np
+import sys
 from utils.data_loader import *
 from utils.data_struct import *
 
-NAME = 'b12'
-SESS = [11, 12, 13, 14]
+# NAME = 'p20'
+# SESS = [49, 50, 51, 52]
+# read in name and session number from command line
+name = sys.argv[1]
+sess = [s for s in sys.argv[2:]]
 
-all_files = ALL_MICE[NAME]
+all_files = ALL_MICE[name]
 
 all_data = []
 for idx, file in enumerate(all_files):
     data_frame = pd.read_csv(file[0], low_memory=False)
-    data = SessionData(NAME, idx, data_frame, file[1])
+    data = SessionData(name, idx, data_frame, file[1])
     all_data.append(data)
 
-for ses_idx in SESS:
+for ses_idx in sess:
 
     data = all_data[ses_idx]
     length = int(data.video.get(cv2.CAP_PROP_FRAME_COUNT))
