a2cdaf7837c602fc7e4e13c3ca21fe182cbf053d
lingqiz <zlqzcc@gmail.com>
2025-03-12 15:47:00 -0400

    minor fix

--- utils/plottools.py
+++ utils/plottools.py
@@ -191,9 +191,9 @@ def pose_to_gifs(pose_frames, frame_rate, pre, center, rotate, gif_filename, exc
                 ax.title.set_text('Time %.1f ms' % (frame_idx / frame_rate * SEC_TO_MS))
             if frame_idx >= int(frame_rate * pre):
                 if center:
-                    ax.scatter(465, 465, s=400, marker='s', color='tab:blue')
+                    ax.scatter(465, -465, s=400, marker='s', color='tab:blue')
                 else:
-                    ax.scatter(975, 975, s=400, marker='s', color='tab:blue')
+                    ax.scatter(975, 50, s=400, marker='s', color='tab:blue')
 
             if center:
                 ax.set_xlim(-512, 512)
